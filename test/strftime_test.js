4aee0d210f38557f138867c2cc416ac7403bd9d8
Christian Johansen <christian@cjohansen.no>
2010-05-09 15:22:50 +0200

    Testing with assert

--- test/strftime_test.js
+++ test/strftime_test.js
@@ -1,7 +1,20 @@
-/*globals console*/
-var date = new Date(2009, 11, 5);
-console.log(date.strftime("%Y"));
-console.log(date.strftime("%m"));
-console.log(date.strftime("%d"));
-console.log(date.strftime("%y"));
-console.log(date.strftime("%F"));
+/*jslint indent: 2*/
+/*globals assert, console*/
+var date = new Date(2009, 9, 2);
+
+try {
+  assert("%Y should return full year",
+         date.strftime("%Y") === "2009");
+  assert("%m should return month",
+         date.strftime("%m") === "10");
+  assert("%d should return date",
+         date.strftime("%d") === "02");
+  assert("%y should return year as two digits",
+         date.strftime("%y") === "09");
+  assert("%F should act as %Y-%m-%d",
+         date.strftime("%F") === "2009-10-02");
+
+  console.log(assert.count + " tests OK");
+} catch (e) {
+  console.log("Test failed: " + e.message);
+}
