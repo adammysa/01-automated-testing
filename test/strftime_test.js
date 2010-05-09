4408ab731b9469157914e301a680fa912fcd2041
Christian Johansen <christian@cjohansen.no>
2010-05-09 15:18:59 +0200

    A simple strftime test case

---
+++ test/strftime_test.js
@@ -0,0 +1,7 @@
+/*globals console*/
+var date = new Date(2009, 11, 5);
+console.log(date.strftime("%Y"));
+console.log(date.strftime("%m"));
+console.log(date.strftime("%d"));
+console.log(date.strftime("%y"));
+console.log(date.strftime("%F"));
